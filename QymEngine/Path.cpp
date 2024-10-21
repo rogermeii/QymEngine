3e6480c8398edec0d4a91c345426b5a824d0db13
rogermeii <myqtmac@126.com>
2024-10-22 00:50:09 +0800

    add path.cpp/h

---
+++ QymEngine/Path.cpp
@@ -0,0 +1,38 @@
+﻿#include "Path.h"
+
+std::string Path::ExecutablePath;
+std::string Path::ExecutableDirectory;
+
+void Path::InitFromArgv(char* argv)
+{
+    ExecutablePath = argv;
+    const size_t last_slash_idx = ExecutablePath.rfind('\\');
+    if (std::string::npos != last_slash_idx)
+    {
+        ExecutableDirectory = ExecutablePath.substr(0, last_slash_idx);
+    }
+}
+
+std::string Path::GetExecutablePath()
+{
+    return ExecutablePath;
+}
+
+std::string Path::GetExecutableDirectory()
+{
+    return ExecutableDirectory;
+}
+
+std::string Path::Combine(const std::string& Dir, const std::string& Name)
+{
+    if (Dir.empty())
+        return Name;
+    if (Name.empty())
+        return Dir;
+
+    std::string Tmp = Dir;
+    if (Tmp[Tmp.length() - 1] != '\\')
+        Tmp += '\\';
+
+    return Tmp + Name;
+}
---
+++ QymEngine/Path.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <string>
+
+class Path
+{
+public:
+    static void InitFromArgv(char* argv);
+
+    static std::string GetExecutablePath();
+    static std::string GetExecutableDirectory();
+
+    static std::string Combine(const std::string& Dir, const std::string& Name);
+
+private:
+    static std::string ExecutablePath;
+    static std::string ExecutableDirectory;
+};
